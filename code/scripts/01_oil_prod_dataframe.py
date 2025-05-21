49095bb596137083c72aaab16d88ece1c68537f0
aleong92 <alvaro.leong92@gmail.com>
2025-05-21 01:36:59 -0600

    Script to load oil production data from SIH and concatenate into a single dataframe with renamed column names

---
+++ code/scripts/01_oil_prod_dataframe.py
@@ -0,0 +1,64 @@
+#####################################################################
+# 01 - OIL PRODUCTION DATAFRAME
+# Author: Álvaro León G.
+# Undergrad Thesis
+# ITAM
+#####################################################################
+# Libraries
+import pandas as pd
+import glob
+import os
+import sys
+
+# Append config to system path and import SIH dictionaries
+sys.path.append(os.path.abspath(os.path.join("..", "..")))
+from config.config import SIH_DTYPES, SIH_PROD_NAMES
+
+# Define PATH and get file names in the folder
+PATH = os.path.join('..', '..', 'data', 'oil_production')
+PROD_FILENAMES = [i for i in glob.glob(PATH+'/prod_*.{}'.format('csv'))]
+
+# Load data and concat into a single dataframe
+df = (
+    pd
+    .concat(
+        [
+            pd
+            .read_csv(
+                f,
+                encoding='latin-1',
+                skiprows=10,
+                dtype=SIH_DTYPES,
+                parse_dates=['Fecha'],
+                date_format="%d-%m-%Y"
+            )
+            for f in PROD_FILENAMES
+        ],
+        ignore_index=True
+    )
+)
+
+# Assign column names
+df.rename(columns=SIH_PROD_NAMES, inplace=True)
+
+# Rename oil basins and erase whitespace
+df.loc[df['cuenca'] == 'CINTURON PLEGADO DE CHIAPAS', 'cuenca'] = 'CHIAPAS'
+df.loc[df['cuenca'] == 'CUENCAS DEL SURESTE', 'cuenca'] = 'SURESTE'
+df.loc[df['nombre_pozo'] == 'EL TREINTA-13DES', 'nombre_pozo'] = 'ELTREINTA-13DES'
+
+# Initial well count
+print(f"""
+      OIL PRODUCTION DATABASE
+      -----------------------
+
+      Wells:      {df['nombre_pozo'].nunique():>7,}
+      Basins:     {df['cuenca'].nunique():>7,}
+
+      -------------------
+
+      Starts:     {(df['fecha'].dt.year).min():>7}
+      Ends:       {(df['fecha'].dt.year).max():>7}
+""")
+
+# Save dataframe
+df.to_csv(os.path.join("..", "..", "data", "oil_intermediate", "01_oil.csv"), sep=',', index=False)
