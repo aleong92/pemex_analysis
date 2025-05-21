5d683e549cd186aac18497d1e27c95dfc7f48d37
aleong92 <alvaro.leong92@gmail.com>
2025-05-21 01:37:53 -0600

    Config and init files to create a dictionary module

---
+++ config/config.py
@@ -0,0 +1,22 @@
+SIH_DTYPES = {
+    'Cuenca': 'object',
+    'Asignación_o_Contrato': 'object',
+    'Nombre_del_pozo': 'object',
+    'Petróleo_(Mbd)': 'float64',
+    'Gas_asociado_(MMpcd)': 'float64',
+    'Gas__no_asociado_(MMpcd)': 'float64',
+    'Agua_(Mbd)': 'float64',
+    'Condensado_(Mbd)': 'float64'
+}
+
+SIH_PROD_NAMES = {
+    'Fecha': 'fecha',
+    'Cuenca': 'cuenca',
+    'Asignación_o_Contrato': 'asignacion_contrato',
+    'Nombre_del_pozo': 'nombre_pozo',
+    'Petróleo_(Mbd)': 'petroleo_mbd',
+    'Gas_asociado_(MMpcd)': 'gas_asociado_mmpcd',
+    'Gas_no_asociado_(MMpcd)': 'gas_no_asociado_mmpcd',
+    'Agua_(Mbd)': 'agua_mbd',
+    'Condensado_(Mbd)': 'condensado_mbd'
+}
